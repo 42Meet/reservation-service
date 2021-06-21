50363fcf81ad92cbf22e008e572abec9c3c4ecea
sebaek42 <sebaek@student.42seoul.kr>
2021-06-21 16:31:51 +0900

    FEAT: member mypage조회 기능 fetch.EAGER버젼
    mypage조회 로직 구현.
    entity의 Participate에서 FetchType을 Lazy에서 Eager로 바꿈.

--- src/main/java/kr/meet42/reservationservice/domain/repository/ParticipateRepository.java
+++ src/main/java/kr/meet42/reservationservice/domain/repository/ParticipateRepository.java
@@ -1,7 +1,11 @@
 package kr.meet42.reservationservice.domain.repository;
 
+import kr.meet42.reservationservice.domain.entity.Member;
 import kr.meet42.reservationservice.domain.entity.Participate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParticipateRepository extends JpaRepository<Participate, Long> {
+    Participate findByMember(Member member);
 }

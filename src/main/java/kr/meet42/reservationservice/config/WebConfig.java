51c2e04199d18c3fa4222755479fc7aa620daaac
JaeminKang <kangjm2@hanyang.ac.kr>
2021-07-02 15:58:46 +0900

    FIX : #88 Modify Configuration for CORS Problem

--- src/main/java/kr/meet42/reservationservice/config/WebConfig.java
+++ src/main/java/kr/meet42/reservationservice/config/WebConfig.java
@@ -2,18 +2,22 @@ package kr.meet42.reservationservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final Environment env;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .exposedHeaders("Authorization");
+                .allowedOrigins(env.getProperty("42meet.server.test"))
+                .allowedMethods("GET, POST, OPTIONS");
     }
 }
